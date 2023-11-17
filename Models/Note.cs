f8361d9d13022061227bab5b037a8b503eaf5a46
johncraik <jgcraik1@sheffield.ac.uk>
2023-11-17 13:50:16 +0000

    Added database to store the todo notes

---
+++ Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CutlerIT_MiniProject.Models;
+
+public partial class Note
+{
+    public long Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Note1 { get; set; }
+
+    public string? Date { get; set; }
+}
