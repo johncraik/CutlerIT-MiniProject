2f1c48ade4a3b3b81c402123aeb3a2ef05c767d2
johncraik <jgcraik1@sheffield.ac.uk>
2023-11-18 20:44:25 +0000

    Added code to test a database insert

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CutlerIT_MiniProject.Models;
 
 namespace CutlerIT_MiniProject.Pages
 {
@@ -15,6 +16,30 @@ namespace CutlerIT_MiniProject.Pages
         public void OnGet()
         {
             //Code runs when page is opened:
+            try
+            {
+                Note note = new Note()
+                {
+                    Title = "Test Insert",
+                    Note1 = "Test insert into the database.",
+                    Date = "20/11/2023"
+                };
+
+                AddNote(note);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void AddNote(Note note)
+        {
+            using(var db = new TodoContext())
+            {
+                db.Notes.Add(note);
+                db.SaveChanges();
+            }
         }
     }
 }
